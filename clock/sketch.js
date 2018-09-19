2d998c87435c9ea26f430e602ce52167cc42227b
CBR0MS <cb@christianbroms.com>
2018-09-18 23:09:45 -0400

    add visuals and number bounce

--- clock/sketch.js
+++ clock/sketch.js
@@ -1,152 +1,156 @@
-class Path {
-  constructor(radius, x, y) {
-    this.radius = radius;
-    this.x = x;
-    this.y = y;
-    this.loopTime = 0;
-    this.currentTime = 0;
-  }
-  set xpos(pos) {
-    this.x = pos;
-  }
-  set ypos(pos) {
-    this.y = pos;
-  }
-  set totalTime(t) {
-    this.loopTime = t;
-  }
-  set curTime(t) {
-    this.currentTime = t;
-  }
-  get px() {
-    let currStep = this.currentTime % this.loopTime;
-    let t = map(currStep, 0, this.loopTime, 0, PI * 2); 
-    return this.x + this.radius * cos(t); 
-  }
-  get py() {
-    let currStep = this.currentTime % this.loopTime;
-    let t = map(currStep, 0, this.loopTime, 0, PI * 2); 
-    return this.y + this.radius * sin(t); 
-  }
+// real time (not based on game)
+let realS, realM, realH; 
+// game time (based on game performance)
+let gameS, gameM, gameH; 
+let p_realS, p_realM, p_realH; 
+// assorted global vars 
+let bufferTime = 6, frequency = 1;
+let deflector, deflectorSpeed = 15, timeSpeed = -10;
+let dispTimeY = 80;
+let times = [];
+
+let font;
+
+function preload() {
+  font = loadFont('digital-7.ttf');
 }
-
-
-class Blob {
-  constructor(centerX, centerY, radius, nodes) {
-    this.centerX = centerX;
-    this.centerY = centerY;
-    this.radius = radius;
-    this.nodes = nodes; this.goX = 0; this.goY = 0;
-    // instance vars 
-    this.nodeStartX = []; this.nodeStartY = [];
-    this.nodeX = []; this.nodeY = [];
-    this.angle = []; this.frequency = [];
-    this.rotAngle = -90; this.organicConstant = 1;
-    this.accelX = 0.0; this.accelY = 0.0;
-    this.deltaX = 0.0; this.deltaY = 0.0;
-    this.springing = 0.0009; this.damping = 0.98;
-    this.col = 0; this.spd = 0.02;
-    // path object to be followed by the blob
-    this.path = new Path(500, this.centerX, this.centerY);
-    // initialize all arrays with 0
-    for (let i = 0; i < this.nodes; i++){
-      this.nodeStartX[i] = 0;
-      this.nodeStartY[i] = 0;
-      this.nodeY[i] = 0;
-      this.nodeY[i] = 0;
-      this.angle[i] = 0;
-    } 
-    // generate random frequency
-    for (let i = 0; i < this.nodes; i++){
-      this.frequency[i] = random(5, 12);
-    }
-  }
-
-  drawShape() {
-    //  calculate node starting locations
-    for (let i = 0; i < this.nodes; i++){
-      this.nodeStartX[i] = this.centerX+cos(radians(this.rotAngle)) * this.radius;
-      this.nodeStartY[i] = this.centerY+sin(radians(this.rotAngle)) * this.radius;
-      this.rotAngle += 360.0/this.nodes;
-    }
-    // draw polygon
-    curveTightness(this.organicConstant);
-    fill(this.col);
-    beginShape();
-    for (let i = 0; i < this.nodes; i++){
-      curveVertex(this.nodeX[i], this.nodeY[i]);
-    }
-    for (let i = 0; i < this.nodes-1; i++){
-      curveVertex(this.nodeX[i], this.nodeY[i]);
-    }
-    endShape(CLOSE);
-  }
-
-  moveShape() {
-   
-    //move center point
-    this.deltaX = this.path.px-this.centerX;
-    this.deltaY = this.path.py-this.centerY;
-    // create springing effect
-    this.deltaX *= this.springing;
-    this.deltaY *= this.springing;
-    this.accelX += this.deltaX;
-    this.accelY += this.deltaY;
-    // move blob center
-    this.centerX += this.accelX * this.spd;
-    this.centerY += this.accelY * this.spd;
-    // slow down springing
-    this.accelX *= this.damping;
-    this.accelY *= this.damping;
-    // change curve tightness
-    this.organicConstant = 1-((abs(this.accelX)+abs(this.accelY))*0.1);
-    // move nodes
-    for (let i = 0; i < this.nodes; i++){
-      this.nodeX[i] = this.nodeStartX[i]+sin(radians(this.angle[i]))*(this.accelX);
-      this.nodeY[i] =this. nodeStartY[i]+sin(radians(this.angle[i]))*(this.accelY);
-      this.angle[i] += this.frequency[i];
-    }
-  }
-  set color(col) {
-    this.col = col;
-  }
-  set speed(spd) {
-    this.spd = spd;
-  }
-  set spring(spring){
-    this.springing = spring;
-  }
-  set dampen(dampen) {
-    this.dampening = dampen;
-  }
-  set rotationFrequency(freq) {
-    this.path.totalTime = freq;
-  }
-  set currentTimeStep(step) {
-    this.path.curTime = step;
-  }
-}
-
-var b, t;
 
 function setup() {
-  createCanvas(800, 800);
-  noStroke();
-  frameRate(30);
-  b = new Blob(height/2, width/2, 45, 30);
-  b.rotationFrequency = 20;
-  b.path.radius = 100;
-  b.color = color(255, 255, 255);
-  b.speed = 2;
-  b.spring = 0.0002;
+  createCanvas(600, 800, P2D);
+  textFont(font);
+  textSize(65);
+  deflector = new Deflector(10, 100, width/2, height/4);
+  t1 = new Time(3, width/4, height);
+  updateRealTime();
+  // to start, game time is the same as real time
+  gameS = realS;
+  gameM = realM;
+  gameH = realH;
 }
 
-function draw() {
-  //fade background
-  let S = second();
-  b.currentTimeStep = S % 20;
-  fill(0, 100);
-  rect(0,0, width, height);
-  b.drawShape();
-  b.moveShape();
+function draw(){
+  fill(205, 0, 0);
+  background(20, 0, 0);
+  updateRealTime();
+  drawGameTime();
+
+  if (p_realS != realS){
+    p_realS = realS;
+    times.push(new Time(realS, random(30, width - 30), height));
+  }
+  for (let i in times) {
+    if (times[i].offscreen()){
+      // remove offscreen times
+      times.splice(i, 1);
+    }
+    if (times[i].intersects(deflector)){
+      // remove intersecting times
+      times[i].bounceMove(random(-10,10), random(6, 12));
+    } 
+    if (times[i].goingUp) {
+      times[i].move(timeSpeed);
+    }
+    times[i].update();
+    times[i].draw();
+  }
+
+  deflector.update();
+  deflector.draw();
+}
+
+function keyReleased() { 
+  if (keyIsDown(RIGHT_ARROW)) { deflector.move(deflectorSpeed) }
+  else if (keyIsDown(LEFT_ARROW)) { deflector.move(-deflectorSpeed) }
+  else { deflector.move(0) }
+}
+
+function keyPressed() {
+  if (keyCode == LEFT_ARROW){ deflector.move(-deflectorSpeed)} 
+  if (keyCode == RIGHT_ARROW){ deflector.move(deflectorSpeed)} 
+}
+
+function updateRealTime() {
+  realS = second();
+  realM = minute();
+  realH = hour();
+}
+
+function drawGameTime() {
+  textAlign(CENTER);
+  text(gameH, width/4, dispTimeY);
+  text(":", width/8 * 3, dispTimeY)
+  text(gameM, width/4 * 2, dispTimeY);
+  text(":", width/8 * 5, dispTimeY)
+  text(gameS, width/4 * 3, dispTimeY);
+}
+
+class Deflector {
+  constructor(h_, w_, x_, y_){
+    this.height = h_;
+    this.width = w_;
+    this.x = x_;
+    this.y = y_;
+    this.deltaX = 0;
+  }
+  update() {
+    this.x += this.deltaX;
+    this.x = constrain(this.x, this.width/2, width - this.width/2);
+  }
+  move(steps) { this.deltaX = steps }
+
+  draw() {
+    //rectMode(CENTER);
+    textAlign(CENTER);
+    //rect(this.x, this.y, this.width, this.height);
+    textSize(70);
+    text("___", this.x, this.y);
+    textSize(65); 
+  }
+}
+
+class Time {
+  constructor(t_, x_, y_) {
+    this.t = t_;
+    this.x = x_;
+    this.y = y_;
+    this.deltaY = 0;
+    this.deltaX = 0;
+    this.goingUp = true;
+  }
+  update() {
+    this.y += this.deltaY;
+    this.x += this.deltaX;
+  }
+  move(steps) { 
+     this.deltaY = steps;
+     //if (this.y > height/4)
+    //else this.deltaY = map(this.y, height/4, dispTimeY, );
+  }
+  bounceMove(stepX, stepY){
+    this.deltaX = stepX;
+    this.deltaY = stepY;
+  }
+
+  intersects(deflector) { 
+    // from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    if (deflector.x < this.x + 50 &&
+        deflector.x + deflector.width > this.x + 40 &&
+        deflector.y < this.y + 10 &&
+        deflector.y + deflector.height > this.y - 40) {
+      this.goingUp = false;
+      this.t = '#'
+      return true;
+    }
+    return false;
+  }
+
+  offscreen() {
+    if (this.y < -40 || this.y > height + 40) return true;
+    else return false;
+  }
+
+  draw() {
+    textAlign(CENTER);
+    text(this.t, this.x, this.y);
+  }
 }
