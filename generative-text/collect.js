ea8b50a161d0380cfbcc472f90744a2b1e22cd9a
CBR0MS <cb@christianbroms.com>
2018-11-12 00:13:34 -0500

    workon getting wiki data

---
+++ generative-text/collect.js
@@ -0,0 +1,43 @@
+let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+let contentUrl = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=';
+
+function preload() {
+    let query = searchUrl + "Glossary_of_philosophy"
+    loadJSON(query, handleData, 'jsonp')
+
+    function handleData(data){
+        let title = data[1][0];
+        title = title.replace(/\s+/g, '_');
+        let page = contentUrl + title
+        let res = loadJSON(page, cleanContent, 'jsonp')
+    }
+
+    function cleanContent(data) {
+        
+        let element = document.createElement("div")
+        element.innerHTML = data.parse.text["*"]
+        let dfns = element.getElementsByTagName('dfn');
+        let all = [];
+        let allUrl = []
+
+        for (let i = 0; i< dfns.length; i++) {
+            let aContent = dfns[i].getElementsByTagName('a');
+            if (aContent[0] != undefined){
+                let content = aContent[0].textContent
+                let url = aContent[0].getAttribute("href")
+                url = url.replace(/(\/wiki\/)/g, "")
+                content = content.replace(/\[[0-9][0-9]?\]/g, "");
+                all.push(content)
+                allUrl.push(url)
+            }
+            
+        }
+        console.log(all)
+        saveJSON(all, 'isms.json');
+        saveJSON(allUrl, 'ismsUrls.json');
+    }
+}
+
+function setup() {
+    console.log("done")
+}
--- generative-text/sketch.js
+++ generative-text/sketch.js
@@ -1,71 +1,154 @@
-let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
-let listSearchUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&qlimit=25&srsearch='
-let contentUrl = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=';
 
-let cleanedData = "";
+let contentUrl = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&redirects&page=';
+
+let singleIsms = [], singleIsmsUrls = []
+let multipleIsms = [], multipleIsmsUrls = []
+let prefixes = []
+
+let generations = 10;
+
+let newPhilosophies = []
+let builtFrom = []
+let sourceTexts = []
 
 function preload () {
 
-    let query = searchUrl + "noam chomsky"
-    //let query = listSearchUrl + "aerospace%20engineers"
-    loadJSON(query, handleData, 'jsonp')
-    // query = searchUrl + "albert einstein";
-    // loadJSON(query, handleData, 'jsonp');
-    // query = searchUrl + "karl popper";
-    // loadJSON(query, handleData, 'jsonp');
-
-    function handleData(data){
-        console.log(data);
-        let title = data[1][0];
-        title = title.replace(/\s+/g, '_');
-        console.log(title)
-        let page = contentUrl + title
-        let res = loadJSON(page, cleanContent, 'jsonp')
-        return res;
-    }
+    loadJSON('prefixes.json', loadPrefixes);
+    loadJSON('ismsUrls.json', loadIsmsUrls);
+    loadJSON('isms.json', loadIsms);
 
     function cleanContent(data) {
-        console.log(data)
         // get html and make new html element 
+        console.log(data)
         let element = document.createElement("div")
         element.innerHTML = data.parse.text["*"]
-        
-
         // get all sup elements 
         let supTags = element.getElementsByTagName("sup")
-
         // remove sup elements 
         while (supTags[0]) supTags[0].parentNode.removeChild(supTags[0])
-
         // get all p elements 
-        let paragraphs = element.getElementsByTagName("p")
-        let collectedData = [];
+        let contents = element.getElementsByClassName("mw-parser-output")[0].childNodes
+        let text = "";
+        for (let i = 0; i < contents.length; i++){
+            if (contents[i].tagName == 'P') {
+                let cont = contents[i].innerText
+                if (!(/^\s*$/g.test(cont))){
+                    text = text + " " + cont
+                }
+            } else if (contents[i].id == 'toc') {
+                i = contents.length
+            }
+        }
+        return text
+    }
 
-        // get the text content of the p elements 
-        for (let i = 0; i < paragraphs.length; i++) {
-            collectedData.push(paragraphs[i].textContent)
+    function loadPrefixes(data) {
+        for (let i = 0; i < data.length; i++){
+            prefixes.push(data[i])
+        }
+    }
+
+    function loadIsmsUrls(data){
+        for (let i = 0; i < data.length; i++) {
+            let splitSpace = data[i].split(" ")
+            let splitDash = data[i].split("-")
+            if (splitSpace.length == 1 && splitDash.length == 1) {
+                singleIsmsUrls.push(data[i])
+            } else {
+                multipleIsmsUrls.push(data[i])
+            }
+        }
+    }
+
+    function loadIsms(data){
+        for (let i = 0; i < data.length; i++) {
+            let splitSpace = data[i].split(" ")
+            let splitDash = data[i].split("-")
+            if (splitSpace.length == 1 && splitDash.length == 1) {
+                singleIsms.push(data[i])
+            } else {
+                multipleIsms.push(data[i])
+            }
+        }
+        createNewPhilosophies(generations)
+    }
+
+    function createNewPhilosophies(n) {
+
+        for (let i = 0; i < (n / 3); i++) {
+            let ind1 = Math.floor(Math.random()*singleIsms.length)
+            let ind2 = Math.floor(Math.random()*multipleIsms.length)
+            let first = singleIsms[ind1]
+            let second = multipleIsms[ind2]
+            let seconds = second.split(" ")
+            if (seconds.length > 1){
+                second = seconds[0]
+                for (let i = 1; i < seconds.length; i++){
+                    second = second + " " + seconds[i].charAt(0).toUpperCase() + seconds[i].substr(1).toLowerCase()
+                }
+            }
+            
+            builtFrom.push({'first': first, 
+                            'firstUrl': singleIsmsUrls[ind1], 
+                            'second': second, 
+                            'secondUrl': multipleIsmsUrls[ind2]})
+            first = first.replace("ism", "ist")
+            newPhilosophies.push(first + " " + second)
         }
 
-        let allData = "";
-        // create one string with the text content 
-        for (let i = 0; i < collectedData.length; i++) {
-            allData = allData + collectedData[i];
+        for (let i = 0; i < (n / 3); i++) {
+            let ind1 = Math.floor(Math.random()*singleIsms.length)
+            let ind2 = Math.floor(Math.random()*singleIsms.length)
+            let first = singleIsms[ind1]
+            let second = singleIsms[ind2]
+            builtFrom.push({'first': first, 
+                            'firstUrl': singleIsmsUrls[ind1], 
+                            'second': second, 
+                            'secondUrl': singleIsmsUrls[ind2]})
+            first = first.replace("ism", "ist")
+            newPhilosophies.push(first + " " + second)
         }
 
-        // create a new html node that contains just the clean text
-        // let element1 = document.createElement("div")
-        // element1.innerHTML = allData
-        // document.getElementById("div1").appendChild(element1);
-        cleanedData = cleanedData + allData;
+        for (let i = 0; i < (n / 3); i++) {
+            let ind1 = Math.floor(Math.random()*singleIsms.length)
+            let ind3 = Math.floor(Math.random()*singleIsms.length)
+            let ind2 = Math.floor(Math.random()*prefixes.length)
+            let first = singleIsms[ind1]
+            let pre = prefixes[ind2]
+            let second = singleIsms[ind3]
+            builtFrom.push({'first': first, 
+                            'firstUrl': singleIsmsUrls[ind1], 
+                            'second': second, 
+                            'secondUrl': singleIsmsUrls[ind3]})
+            first = first.replace("ism", "ist")
+            pre = pre.charAt(0).toUpperCase() + pre.substr(1).toLowerCase()
+            newPhilosophies.push(pre + first + " " + second)
+        }
+        getWikiSummaries()
+        console.log(newPhilosophies)
+    }
+
+    function getWikiSummaries() {
+        for (let i = 0; i < builtFrom.length; i++) {
+            let res = loadJSON((contentUrl + builtFrom[i].firstUrl), (data) => {
+                let cleaned = cleanContent(data)
+                let res = loadJSON((contentUrl + builtFrom[i].secondUrl), (data) => {
+                    let cleaned2 = cleanContent(data)
+                    sourceTexts.push(cleaned + " " + cleaned2);
+                }, 'jsonp')
+            }, 'jsonp')
+        }
     }
 }
 
+
+
 function setup(){
 
-    makeMarkov(cleanedData);
+    makeMarkov(sourceTexts[0]);
 
      function makeMarkov(data){
-        let rm = new RiMarkov(4);
+        let rm = new RiMarkov(3);
         rm.loadText(data);
         let sentences = rm.generateSentences(12);
         let allSentences = "";
